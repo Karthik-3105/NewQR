33f54e1e3fd022ecd557abf9d6b85fd95c980251
OmAmberkar <omamberkar08@gmail.com>
2025-01-03 20:45:20 +0530

    Initialize React project with Vite and Tailwind CSS setup

---
+++ QRcodeAttendance/src/App.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+function App() {
+  return (
+    <div className='bg-slate-900 text-yellow-400 underline font-bold italic hover:not-italic text-center mt-14 m-8'>
+      Hello guys lets work on Qr code attendance
+    </div>
+  )
+}
+
+export default App
